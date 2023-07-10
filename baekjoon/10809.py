aa0fb3b470bec8f31c553b4336ca69c7411f2c3e
jimizip <rnwlals02@naver.com>
2023-07-10 16:27:38 +0900

    10809 수정

--- baekjoon/10809.py
+++ baekjoon/10809.py
@@ -1,10 +1,7 @@
 import string
 
 lower = [i for i in string.ascii_lowercase]
-S = list(input())
+S = input()
 for i in lower:
-    if i in S:
-        print(S.index(i), end=" ")
-    else:
-        print(-1, end=" ")
-
+    print(S.find(i), end=" ")
+    
