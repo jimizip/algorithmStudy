3921ab36a6aa26d3f06400cb0b8af0b0708c3a0e
jimizip <rnwlals02@naver.com>
2023-07-29 03:08:36 +0900

    1157 성공

--- baekjoon/1157.py
+++ baekjoon/1157.py
@@ -1,8 +1,13 @@
 word = input().upper()
 word_list = list(set(word))
-count_list = [word.count(i) for i in word_list]
+count_list = []
 
-if count_list.count(max(count_list)):
+for i in word_list:
+    cnt = word.count(i)
+    count_list.append(cnt)
+
+if count_list.count(max(count_list)) > 1:
     print("?")
 else:
-    print(word_list[count_list.index(max(count_list))])
+    res = count_list.index(max(count_list))
+    print(word_list[res])
