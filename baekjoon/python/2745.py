b095387d60a445ac7a6ef8fe371cf0e06c2799e4
jimizip <rnwlals02@naver.com>
2024-10-31 15:29:47 +0900

    2745

---
+++ baekjoon/python/2745.py
@@ -0,0 +1,11 @@
+N, B = input().split()
+# num_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# result = 0
+
+# for i, num in enumerate(N[::-1]):
+#     result += int(B) ** i * num_list.index(num)
+
+# print(result)
+
+print(int(N, int(B)))
