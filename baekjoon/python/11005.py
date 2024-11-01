b6da295b4793bb07658c300eb2d8dfc0f3913c32
jimizip <rnwlals02@naver.com>
2024-11-02 00:12:54 +0900

    modified 11005

--- baekjoon/python/11005.py
+++ baekjoon/python/11005.py
@@ -1,10 +1,25 @@
-N, B = map(int, input().split())
+# N, B = map(int, input().split())
+# arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# result = ""
+
+# while N:
+#     result += arr[N%B]
+#     N //= B
+
+# print(result[::-1])
+
 arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-result = ""
 
-while N:
-    result += arr[N%B]
-    N //= B
+def convert(N, B):
+    if N == 0:
+        return "0"
+    
+    result = []
+    while N:
+        result.append(arr[N % B])
+        N //= B
+    
+    return ''.join(result[::-1])
 
-print(result)
-
+N, B = map(int, input().split())
+print(convert(N, B))
