8e6da2a30f94281744d5dc3fe41e6f447432ceb2
jimizip <rnwlals02@naver.com>
2024-11-11 23:36:29 +0900

    7568

---
+++ baekjoon/python/7568.py
@@ -0,0 +1,13 @@
+N = int(input())
+student = []
+
+for _ in range(1, N+1):
+    x, y = map(int, input().split())
+    student.append((x,y))
+
+for i in student:
+    rank = 1
+    for j in student:
+        if i[0] < j[0] and i[1] < j [1]:
+            rank += 1
+    print(rank, end=" ")
